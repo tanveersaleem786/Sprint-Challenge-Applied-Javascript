6ed7c4e871f3bb689c8fb7e8e8c77aaa10d3e8cf
tanveersaleem786 <tanveer.saleem786@gmail.com>
2019-12-13 19:41:42 -0500

    Card section completed

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,59 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function createCard(article)
+{
+   let card = document.createElement('div');
+   card.classList.add('card');
+
+   let headline = document.createElement('div');
+   headline.classList.add('headline');
+
+   let author = document.createElement('div');
+   author.classList.add('author');
+
+   let container = document.createElement('div');
+   container.classList.add('img-container');
+
+   let img = document.createElement('img');
+   let name = document.createElement('span');
+
+   card.appendChild(headline);
+   card.appendChild(author); 
+   author.appendChild(container);
+   container.appendChild(img);
+   author.appendChild(name);   
+
+   headline.textContent = article.headline;
+   img.src = article.authorPhoto;
+   name.textContent = article.authorName;  
+
+   let cards = document.querySelector('.cards-container');
+   cards.appendChild(card);
+}
+
+
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+       
+    .then( response => {
+        
+        let obj = response.data.articles;             
+        var result = Object.entries(obj);  
+        let total = result.length; 
+        
+        for(let i=0; i<total; i++) {
+            
+            let finalArr = result[i][1];               
+            finalArr.forEach(article => {
+            createCard(article);
+            });
+
+        }
+        
+    })
+    .catch( err => {
+        console.log("Error:", err);
+    })
